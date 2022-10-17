1493e96e84bba533bdfa25584354b7e06162c0a6
Alexey Fortunatov <kekray777@gmail.com>
2022-10-17 16:08:57 +0400

    Added Navbar and its styled

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaRProject, FaTimes } from "react-icons/fa";
 import { CgMenuRight } from "react-icons/cg";
 import { IconContext } from "react-icons";
@@ -13,9 +13,58 @@ import {
   NavItem,
 } from "./NavbarStyles";
 import { useLocation, useNavigate } from "react-router-dom";
+import { data } from "../../data/NavbarData";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  const [show, setShow] = useState(false);
+
+  let history = useNavigate();
+  let location = useLocation();
+
+  const handleClick = () => {
+    setShow(!show);
+  };
+
+  const scrollTo = (id) => {
+    const element = document.getElementById(id);
+
+    element.scrollIntoView({
+      behavior: "smooth",
+    });
+  };
+
+  const closeMobileMenu = (to, id) => {
+    if (id && location.pathname === "/") {
+      scrollTo(id);
+    }
+
+    history.push(to);
+    setShow(false);
+  };
+
+  return (
+    <IconContext.Provider value={{ color: "#fff" }}>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">
+            <NavIcon src="../../assets/logo.png" alt="logo">
+              Delta
+            </NavIcon>
+          </NavLogo>
+          <MobileIcon>{show ? <FaTimes /> : <CgMenuRight />}</MobileIcon>
+          <NavMenu>
+            {data.map((el, index) => (
+              <NavItem key={index}>
+                <NavLinks onClick={() => closeMobileMenu(el.to, el.id)}>
+                  {el.text}
+                </NavLinks>
+              </NavItem>
+            ))}
+          </NavMenu>
+        </NavbarContainer>
+      </Nav>
+    </IconContext.Provider>
+  );
 }
 
 export default Navbar;
