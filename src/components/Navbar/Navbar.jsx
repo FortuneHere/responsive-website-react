36d73756458665f372c31e24704d3a4c65f38786
Alexey Fortunatov <kekray777@gmail.com>
2022-10-17 15:32:36 +0400

    Added data for components

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { FaRProject, FaTimes } from "react-icons/fa";
+import { CgMenuRight } from "react-icons/cg";
+import { IconContext } from "react-icons";
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  NavIcon,
+  MobileIcon,
+  NavMenu,
+  NavLinks,
+  NavItem,
+} from "./NavbarStyles";
+import { useLocation, useNavigate } from "react-router-dom";
+
+function Navbar() {
+  return <div>Navbar</div>;
+}
+
+export default Navbar;
